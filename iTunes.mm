bef9e43b8b8fca12be06b6a509ae93cebc26de88
alphaKAI <alpha.kai.net@alpha-kai-net.info>
2016-12-19 03:58:07 +0900

    fix memory bug

--- iTunes.mm
+++ iTunes.mm
@@ -49,22 +49,13 @@ extern "C" {
     SBElementArray<iTunesArtwork*>* artworks = [current artworks];
     Artwork*                        artwork  = NULL;
 
-    {
-      bool flag = true;
 
-      for (iTunesArtwork* _artwork in artworks) {
-        artwork         = (Artwork*)xmalloc(sizeof(Artwork));
-        artwork->length = [[_artwork rawData] length];
-        artwork->data   = (unsigned char*)xmalloc(artwork->length);
+    for (iTunesArtwork* _artwork in artworks) {
+      artwork         = (Artwork*)xmalloc(sizeof(Artwork));
+      artwork->length = [[_artwork rawData] length];
+      artwork->data   = (unsigned char*)xmalloc(artwork->length);
 
-        memcpy(artwork->data, [[_artwork rawData] bytes], artwork->length);
-
-        flag = false;
-      }
-
-      if (flag) {
-        artwork->data = NULL;
-      }
+      memcpy(artwork->data, [[_artwork rawData] bytes], artwork->length);
     }
 
     music->artwork = artwork;
@@ -73,8 +64,10 @@ extern "C" {
   }
 
   void freeMusic(Music* music) {
-    free(music->artwork->data);
-    free(music->artwork);
+    if (music->artwork != NULL) {
+      free(music->artwork->data);
+      free(music->artwork);
+    }
     free(music);
   }
 }
