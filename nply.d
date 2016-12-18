9d08248643b703714e84b55609540a30f748a2a9
alphaKAI <alpha.kai.net@alpha-kai-net.info>
2016-12-19 04:35:10 +0900

    use kqueue to observe music change

--- nply.d
+++ nply.d
@@ -1,3 +1,9 @@
+import kqueuez,
+       core.sys.posix.sys.time,
+       core.sys.posix.unistd;
+import core.thread;
+import core.stdc.stdlib,
+       core.stdc.string;
 import std.algorithm,
        std.base64,
        std.string,
@@ -6,7 +12,6 @@ import std.algorithm,
        std.stdio,
        std.json,
        std.conv;
-import core.stdc.string;
 import twitter4d;
 
 extern(C) {
@@ -26,59 +31,148 @@ extern(C) {
   void freeMusic(Music* music);
 }
 
-
-void main() {
+struct Env {
   Twitter4D t4d;
-  Music* music;
+  Music*    music;
+  string currentMusic,
+         previousMusic;
+}
 
-  // Please replace with your keys before use this software
-  string[string] keys = [
-    "consumerKey"       : "",
-    "consumerSecret"    : "",
-    "accessToken"       : "",
-    "accessTokenSecret" : ""
-  ];
+static Env E;
 
-  music = getCurrentiTunesPlay();
+private static string getJsonData(JSONValue parsedJson, string key) {
+  return parsedJson.object[key].str;
+}
 
-  if (music == null) {
-    throw new Error("[Fatal Error] - iTunes is not running.");
+private static string readFile(string filePath) {
+  auto file = File(filePath, "r");
+  string buf;
+
+  foreach(line; file.byLine) {
+    buf = buf ~ cast(string)line;
   }
 
-  t4d = new Twitter4D(keys);
-  string name   = cast(string)fromStringz(music.name),
-         album  = cast(string)fromStringz(music.album),
-         artist = cast(string)fromStringz(music.artist);
-  string nowPlayingString = "Now Playing: " ~ name ~ " from " ~ album ~ " (" ~ artist ~ ") #NowPlaying";
+  return buf;
+}
 
-  writeln("NowPlaying:");
-  writefln("name   : %s", name);
-  writefln("album  : %s", album);
-  writefln("artist : %s", artist);
-  writeln;
+private static string[string] buildAuthHash(JSONValue parsed) {
+  return [
+            "consumerKey"       : getJsonData(parsed, "consumerKey"),
+            "consumerSecret"    : getJsonData(parsed, "consumerSecret"),
+            "accessToken"       : getJsonData(parsed, "accessToken"),
+            "accessTokenSecret" : getJsonData(parsed, "accessTokenSecret")
+  ];
+}
 
-  writeln("[Tweet] - ", nowPlayingString);
+private static void init() {
+  auto keys = parseJSON(readFile("settings.json")).buildAuthHash;
 
-  Artwork* artwork = music.artwork;
+  E.t4d = new Twitter4D(keys);
+}
 
-  if (artwork !is null) {
-    ubyte[] buf;
-    buf.length = artwork.length;
-    memcpy(buf.ptr, artwork.data, buf.length);
+private static Music* checkTrackChange() {
+  with (E) {
+    music = getCurrentiTunesPlay;
 
-    string encoded = Base64.encode(buf);
+    if (music is null) {
+      writeln("iTunes is playing music.");
 
-    auto parsed = parseJSON((t4d.customUrlRequest("https://upload.twitter.com/1.1/", "POST", "media/upload.json", ["media_data": encoded])));
+      return null;
+    }
 
-    if ("media_id_string" in parsed.object) {
-      string media_id = parsed.object["media_id_string"].str;
-      t4d.request("POST", "statuses/update.json", ["status" : nowPlayingString, "media_ids" : media_id]);
+    currentMusic = cast(string)fromStringz(music.name).idup;
+
+    if (currentMusic != previousMusic) {
+      return music;
+    } else {
+      return null;
+    }
+  }
+}
+
+private static void tweet() {
+  with (E) {
+    music = checkTrackChange;
+    if (music is null) {
+      return;
+    }
+
+    string name   = cast(string)fromStringz(music.name),
+           album  = cast(string)fromStringz(music.album),
+           artist = cast(string)fromStringz(music.artist);
+    string nowPlayingString = "Now Playing: " ~ name ~ " from " ~ album ~ " (" ~ artist ~ ") #NowPlaying";
+
+    currentMusic  = name.idup;
+    previousMusic = name.idup;
+
+    writeln("NowPlaying:");
+    writefln("name   : %s", name);
+    writefln("album  : %s", album);
+    writefln("artist : %s", artist);
+    writeln;
+
+    writeln("[Tweet] - ", nowPlayingString);
+
+    Artwork* artwork = music.artwork;
+
+    if (artwork !is null) {
+      ubyte[] buf;
+      buf.length = artwork.length;
+      memcpy(buf.ptr, artwork.data, buf.length);
+
+      string encoded = Base64.encode(buf);
+
+      auto parsed = parseJSON((t4d.customUrlRequest("https://upload.twitter.com/1.1/", "POST", "media/upload.json", ["media_data": encoded])));
+
+      if ("media_id_string" in parsed.object) {
+        string media_id = parsed.object["media_id_string"].str;
+
+        t4d.request("POST", "statuses/update.json", ["status" : nowPlayingString, "media_ids" : media_id]);
+      } else {
+        t4d.request("POST", "statuses/update.json", ["status" : nowPlayingString]);
+      }
+      
     } else {
       t4d.request("POST", "statuses/update.json", ["status" : nowPlayingString]);
     }
-  } else {
-    t4d.request("POST", "statuses/update.json", ["status" : nowPlayingString]);
+
+    freeMusic(music);
+  }
+}
+
+void diep(string msg) {
+  stderr.writeln("[ERROR] ", msg);
+  exit(EXIT_FAILURE);
+}
+
+void main() {
+  init;
+
+  kevent_t change,
+           event;
+  int kq, nev;
+
+  if ((kq = kqueue()) == -1) {
+    diep("kqueue()");
   }
 
-  freeMusic(music);
+  EV_SET(&change, 1, EVFILT_TIMER, EV_ADD | EV_ENABLE, 0, 5000, null);
+
+  for (;;) {
+    nev = kevent(kq, &change, 1, &event, 1, null);
+
+    if (nev < 0) {
+      diep("kevent()");
+    } else if (nev > 0) {
+      if (event.flags & EV_ERROR) {
+        stderr.writefln("EV_ERROR: %s", strerror(cast(int)event.data).fromStringz);
+        exit(EXIT_FAILURE);
+      }
+
+      tweet;
+    }
+  }
+
+  close(kq);
+  exit(EXIT_SUCCESS);
 }
--- readme.md
+++ readme.md
@@ -13,8 +13,19 @@ NowPlaying tweet tool in D with OS X's iTunes(using Scripting Bridge).
 ###1. Clone this repository
 `$ git clone https://github.com/alphaKAI/nply`
 
-###2. Replace with your keys in nply.d
-`$ (your favorite editor) nply.d`
+###2. Write your your keys in settings.json
+`$ (your favorite editor) settings.json`
+
+Please configure as follows:  
+
+```json:settings.json
+{
+  "consumerKey"       : "Your Consumer Key",
+  "consumerSecret"    : "Your Consumer Secret",
+  "accessToken"       : "your Access Token",
+  "accessTokenSecret" : "Your Access Token Secret"  
+}
+```
 
 ###3. Generate header of iTunes.h with sdef command
 `$ sdef /Applications/iTunes.app | sdp -fh --basename iTunes`
@@ -23,7 +34,7 @@ NowPlaying tweet tool in D with OS X's iTunes(using Scripting Bridge).
 
 ```zsh
 $ gcc -c iTunes.mm
-$ dmd nply.d twitter4d.d iTunes.o -L-framework -LFoundation -L-framework -LiTunesLibrary -L-framework -LScriptingBridge
+$ dmd nply.d twitter4d.d iTunes.o kqueuez.d -L-framework -LFoundation -L-framework -LiTunesLibrary -L-framework -LScriptingBridge
 ```
 
 Complete!  
